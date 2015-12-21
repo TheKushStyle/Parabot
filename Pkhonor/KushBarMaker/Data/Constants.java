7890aab4b8cd7f007c9734ad67cb4e7daa731e0d
TheKushStyle <Frank.boersma@hotmail.com>
2015-12-21 18:07:14 +0100

    BarMaker

---
+++ Pkhonor/KushBarMaker/Data/Constants.java
@@ -0,0 +1,26 @@
+package KushBarMaker.Data;
+
+public class Constants {
+    public final static int CopperOre = 437;
+    public final static int TinOre = 439;
+    public final static int IronOre = 441;
+    public final static int SilverOre = 443;
+    public final static int CoalOre = 454;
+    public final static int GoldOre = 445;
+    public final static int MithrilOre = 448;
+    public final static int AdamantOre = 450;
+    public final static int RuneOre = 452;
+
+    public final static int BronzeBar = 2350;
+    public final static int IronBar = 2352;
+    public final static int SteelBar = 2354;
+    public final static int SilverBar = 2356;
+    public final static int GoldBar = 2358;
+    public final static int MithrilBar =  2360;
+    public final static int AdamantBar = 2362;//probably
+    public final static int RuneBar = 2364;//probably
+
+    public final static int Furnace = 2781;
+    public final static int FurnaceSkill = 2643;
+    public final static int DepositChest = 3193;
+}
